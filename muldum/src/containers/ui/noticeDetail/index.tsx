5838a81e6385db399c78644eff8262dcef58634d
dpqlsn <a01037111209@gmail.com>
2025-09-26 00:01:34 +0900

    feat(#15) :: 디테일 화면 추가

---
+++ muldum/src/containers/ui/noticeDetail/index.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import * as _ from './style';
+
+export default function MoticeDetail(){
+    return (
+        <_.Container>
+            디데틸화면이에옹
+        </_.Container>
+    );
+}
---
+++ muldum/src/containers/ui/noticeDetail/style.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-height: 85vh;
+    row-gap: 2rem;
+`;
