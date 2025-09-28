4c75b8a611b73d8111004cd6c424b68552c4f46a
dpqlsn <a01037111209@gmail.com>
2025-09-28 17:39:00 +0900

    feat (#19) :: 404 페이지 퍼블리싱

--- muldum/src/app/not-found.tsx
+++ muldum/src/app/not-found.tsx
@@ -1,10 +1,65 @@
-import Link from "next/link";
+"use client";
 
-export default function NotFound(){
+import Link from "next/link";
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+
+export default function NotFound() {
     return (
-        <>
-            <h1>404</h1>
-            <Link href='/'>홈으로</Link>
-        </>
+        <Container>
+            <Wrapper>
+                <Title>404</Title>
+                <Subtitle>존재하지 않는 페이지입니다</Subtitle>
+            </Wrapper>
+            <HomeLink href="/">홈으로 돌아가기</HomeLink>
+        </Container>
     );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 70vh;
+  row-gap: 3rem;
+  text-align: center;
+`;
+
+const jejuFont = css`
+  @font-face {
+    font-family: 'JejuStoneWall';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2210-EF@1.0/EF_jejudoldam.woff2') format('woff2');
+    font-weight: normal;
+    font-display: swap;
+  }
+`;
+
+const Title = styled.div`
+  ${jejuFont}
+  font-family: 'JejuStoneWall', sans-serif;
+  font-size: 7.5rem;
+  font-weight: 500;
+`;
+
+const Subtitle = styled.div`
+  font-size: 2.25rem;
+  font-weight: 600;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    row-gap: 1rem;
+`;
+
+const HomeLink = styled(Link)`
+  font-size: 1rem;
+  color: #909090;
+
+  &:hover {
+    color: #545454;
+  }
+`;
