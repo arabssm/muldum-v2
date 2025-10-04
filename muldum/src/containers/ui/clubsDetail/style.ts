5eeece7fb860d5a1bff3bdae0c80579046c2094f
dpqlsn <a01037111209@gmail.com>
2025-10-04 19:43:24 +0900

    feat(#21) :: 퍼블리싱

--- muldum/src/containers/ui/clubsDetail/style.ts
+++ muldum/src/containers/ui/clubsDetail/style.ts
@@ -2,8 +2,63 @@ import styled from "@emotion/styled";
 
 export const Container = styled.div`
     display: flex;
-    flex-direction: column;
     row-gap: 3.5rem;
     width: 100%;
     margin: 0 auto;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    row-gap: 1.2rem;
+    width: 40%;
+`;
+
+export const Group = styled.div`
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.8rem;
+`;
+
+export const Content = styled.div`
+    display: flex;
+    font-size: 1.25rem;
+    color: #B2B2B2;
+`;
+
+export const Title = styled.div`
+    font-size: 1.75rem;
+    font-weight: 600;
+`;
+
+export const PointGroup = styled.div`
+    display: flex;
+    gap: 1rem;
+`;
+
+export const Point = styled.div`
+    display: flex;
+    align-items: center;
+    font-size: 1.075rem;
+    color: #FF9B62;
+    gap: 0.4rem;
+`;
+
+export const Box = styled.div`
+    display: flex;
+    width: 100%;
+    height: 40vh;
+    background-color: #FF9B62;
+`;
+
+export const ContentGroup = styled.div`
+    display: flex;
+    flex-direction: column;
+    row-gap: 1.2rem;
+    width: 100%;
+`;
+
+export const Text = styled.div`
+    display: flex;
+    font-size: 1.25rem;
 `;
