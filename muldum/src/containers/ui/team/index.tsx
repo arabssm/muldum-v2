76a10e54fab44dfe082affcf82148c789cbf810a
dpqlsn <a01037111209@gmail.com>
2025-10-18 18:33:16 +0900

    feat (#23) :: 구색 맞춤

--- muldum/src/containers/ui/team/index.tsx
+++ muldum/src/containers/ui/team/index.tsx
@@ -1,9 +1,42 @@
 import * as _ from './style'
 
-export default function Team(){
-    return(
+export default function Team() {
+    return (
         <_.Container>
-            
+            <_.Wrapper>
+                <_.Text>전공동아리</_.Text>
+                <_.Group>
+                    <_.Box>
+                        <_.Name>아라</_.Name>
+                        <_.Member>학번이름 학번이름 <br /> 학번이름 학번이름 학번이름 학번이름</_.Member>
+                    </_.Box>
+                    <_.Box>
+                        <_.Name>아라</_.Name>
+                        <_.Member>학번이름 학번이름 <br /> 학번이름 학번이름 학번이름 학번이름</_.Member>
+                    </_.Box>
+                    <_.Box>
+                        <_.Name>아라</_.Name>
+                        <_.Member>학번이름 학번이름 <br /> 학번이름 학번이름 학번이름 학번이름</_.Member>
+                    </_.Box>
+                </_.Group>
+            </_.Wrapper>
+            <_.Wrapper>
+                <_.Text>자율동아리</_.Text>
+                <_.Group>
+                    <_.Box>
+                        <_.Name>아라</_.Name>
+                        <_.Member>학번이름 학번이름 <br /> 학번이름 학번이름 학번이름 학번이름</_.Member>
+                    </_.Box>
+                    <_.Box>
+                        <_.Name>아라</_.Name>
+                        <_.Member>학번이름 학번이름 <br /> 학번이름 학번이름 학번이름 학번이름</_.Member>
+                    </_.Box>
+                    <_.Box>
+                        <_.Name>아라</_.Name>
+                        <_.Member>학번이름 학번이름 <br /> 학번이름 학번이름 학번이름 학번이름</_.Member>
+                    </_.Box>
+                </_.Group>
+            </_.Wrapper>
         </_.Container>
     );
 }
--- muldum/src/containers/ui/team/style.ts
+++ muldum/src/containers/ui/team/style.ts
@@ -2,7 +2,48 @@ import styled from "@emotion/styled";
 
 export const Container = styled.div`
     display: flex;
-    row-gap: 3.5rem;
+    flex-direction: column;
     width: 100%;
+    row-gap: 5rem;
     margin: 0 auto;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 2rem;
+`;
+
+export const Text = styled.div`
+    display: flex;
+    font-size: 1.5rem;
+    font-weight: 600;
+`;
+
+export const Group = styled.div`
+    display: flex;
+    gap: 1.25rem;
+    width: 100%;
+`;
+
+export const Box = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0.8rem;
+    padding: 2rem 1.8rem;
+    width: 25%;
+    background-color: #FAFAFA;
+    border-radius: 4px;
+`;
+
+export const Name = styled.div`
+    display: flex;
+    font-size: 1.25rem;
+    font-weight: 500;
+`;
+
+export const Member = styled.div`
+    display: flex;
+    font-size: 1rem;
+    color: #909090;
 `;
