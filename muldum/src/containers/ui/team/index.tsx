1d0278f5d598b6142d1c1fdb61dc0d73e2ecda74
dpqlsn <a01037111209@gmail.com>
2025-10-18 18:21:07 +0900

    feat (#23) :: 기본 라우팅

---
+++ muldum/src/containers/ui/team/index.tsx
@@ -0,0 +1,9 @@
+import * as _ from './style'
+
+export default function Team(){
+    return(
+        <_.Container>
+            
+        </_.Container>
+    );
+}
---
+++ muldum/src/containers/ui/team/style.ts
@@ -0,0 +1,8 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+    display: flex;
+    row-gap: 3.5rem;
+    width: 100%;
+    margin: 0 auto;
+`;
