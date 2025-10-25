4914ad6089e5713a7764921e1c8f2730b599c309
dpqlsn <a01037111209@gmail.com>
2025-10-25 17:25:14 +0900

    docs(#31) :: 필수 추가

--- src/widgets/monthlyTest/data.ts
+++ src/widgets/monthlyTest/data.ts
@@ -1,20 +1,20 @@
 export const sections = [
   {
-    title: '주제',
+    title: ' 주제 (필수)',
     placeholders: [
       '프로젝트 구현 시 적용 기술 작성, 스터디 동아리일 경우 스터디 주제 작성',
     ],
   },
   {
-    title: '목표',
+    title: '목표 (필수)',
     placeholders: ['1.', '2.'],
   },
   {
-    title: '사용언어 · 기술스택',
+    title: '사용언어 · 기술스택 (필수)',
     placeholders: ['프로젝트 구현 사용한 언어나 기술스택을 적어주세요'],
   },
   {
-    title: '발생문제와 어려웠던 점, 해결방안',
+    title: '발생문제와 어려웠던 점, 해결방안 (필수)',
     placeholders: ['신청 사유를 10자 이상 입력해주세요'],
     isTextarea: true,
   },
