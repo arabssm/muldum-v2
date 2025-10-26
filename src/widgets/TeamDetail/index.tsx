22508728a8e99a8054a9af7f318f14404aa7606c
dpqlsn <a01037111209@gmail.com>
2025-10-26 17:11:28 +0900

    chore(#33) :: 화상통화 클릭 시 이동 로직

--- src/widgets/TeamDetail/index.tsx
+++ src/widgets/TeamDetail/index.tsx
@@ -7,36 +7,32 @@ import Group from "@/components/group/teams";
 import MonthlyTest from "../monthlyTest";
 import Calendar from "../calendar";
 import Notion from "../notion";
+import VideoChat from "../videoChat";
 
-const LockedGroups = ["화상통화"] as const;
 type GroupType = "공유캘린더" | "월말평가" | "화상통화" | "노션";
 
 export default function Items() {
     const [active, setActive] = useState<GroupType | null>(null);
-    const [lockedMessage, setLockedMessage] = useState<string>("");
+    const [, setLockedMessage] = useState<string>("");
 
     return (
         <_.Container>
             <Group active={active} setActive={setActive} setMessage={setLockedMessage} />
 
-            {LockedGroups.includes(active as typeof LockedGroups[number]) ? (
-                <_.MessageWrapper>
-                    <Image src="/assets/nob.svg" alt="No" width={120} height={120} />
-                    <_.Message>{lockedMessage}</_.Message>
-                </_.MessageWrapper>
-            ) : active === "월말평가" ? (
+            {active === "월말평가" ? (
                 <MonthlyTest />
             ) : active === "공유캘린더" ? (
                 <Calendar />
             ) : active === "노션" ? (
                 <Notion />
-            ) :
-                (
-                    <_.MessageWrapper>
-                        <Image src="/assets/choice.svg" alt="No" width={120} height={120} />
-                        <_.Message>원하는 기능을 선택하세요</_.Message>
-                    </_.MessageWrapper>
-                )}
+            ) : active === "화상통화" ? (
+                <VideoChat />
+            ) : (
+                <_.MessageWrapper>
+                    <Image src="/assets/choice.svg" alt="No" width={120} height={120} />
+                    <_.Message>원하는 기능을 선택하세요</_.Message>
+                </_.MessageWrapper>
+            )}
         </_.Container>
     );
 }
--- src/widgets/videoChat/index.tsx
+++ src/widgets/videoChat/index.tsx
@@ -3,7 +3,7 @@ import * as _ from './style';
 export default function VideoChat(){
     return(
         <_.Container>
-            
+            dd
         </_.Container>
     );
 }
