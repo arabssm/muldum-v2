08672067861c81bacbf46c371f262bf047a9cebe
dpqlsn <a01037111209@gmail.com>
2025-11-04 09:37:58 +0900

    feat(#70) :: 공지사항 작성 페이지 퍼블리싱

--- src/widgets/teacher/noticeWrite/index.tsx
+++ src/widgets/teacher/noticeWrite/index.tsx
@@ -1,9 +1,32 @@
 import * as _ from "./style";
+import { BtnPrimary, BtnSecondary } from "@/shared/ui/button";
 
 export default function noticeWrite() {
     return (
         <_.Container>
-            공지 작성 페이지
+            <_.Group>
+                <_.Title>공지사항 작성</_.Title>
+                <_.Wrapper>
+                    <_.SubTitle>제목</_.SubTitle>
+                    <_.Input type="text" placeholder="공지사항 제목을 입력해주세요." />
+                </_.Wrapper>
+                <_.Wrapper>
+                    <_.SubTitle>기한</_.SubTitle>
+                    <_.DateInput type="date" />
+                </_.Wrapper>
+                <_.Wrapper>
+                    <_.SubTitle>내용</_.SubTitle>
+                    <_.TextArea placeholder="/를 눌러 다양한 기능을 사용하세요." />
+                </_.Wrapper>
+                <_.Wrapper>
+                    <_.SubTitle>첨부파일</_.SubTitle>
+                    <_.FileDrop>클릭하여 파일을 넣거나 드래그 하세요.</_.FileDrop>
+                </_.Wrapper>
+                <_.BtnGroup>
+                    <BtnSecondary>취소</BtnSecondary>
+                    <BtnPrimary>작성하기</BtnPrimary>
+                </_.BtnGroup>
+            </_.Group>
         </_.Container>
     );
 }
--- src/widgets/teacher/noticeWrite/style.ts
+++ src/widgets/teacher/noticeWrite/style.ts
@@ -3,6 +3,90 @@ import styled from "@emotion/styled";
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: center;
     min-height: 85vh;
-    row-gap: 2rem;
+`;
+
+export const Group = styled.div`
+    display: flex;
+    gap: 2.5rem;
+    width: 80%;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    align-items: flex-start;
+    gap: 2rem;
+    width: 100%;
+`;
+
+export const BtnGroup = styled.div`
+    width: 100%;
+    justify-content: end;
+    display: flex;
+    gap: 1rem;
+`
+
+export const Title = styled.div`
+    width: 100%;
+    font-size: 1.5rem;
+    font-weight: 500;
+`;
+
+export const SubTitle = styled.div`
+    font-size: 1.4rem;
+    font-weight: 500;
+    min-width: 8%;
+    display: flex;
+    align-items: center;
+`;
+
+export const Input = styled.input`
+    width: 60%;
+    padding: 0.8rem;
+    border: 1px solid #D1D1D1;
+    border-radius: 4px;
+    font-size: 1rem;
+    outline: none;
+
+    ::placeholder {
+        color: #D1D1D1;
+    }
+`;
+
+export const DateInput = styled.input`
+    padding: 0.6rem 0.8rem;
+    border: 1px solid #D1D1D1;
+    border-radius: 4px;
+    font-size: 1rem;
+    outline: none;
+`;
+
+export const TextArea = styled.textarea`
+    width: 100%;
+    height: 25vh;
+    padding: 1rem;
+    border: 1px solid #D1D1D1;
+    border-radius: 4px;
+    font-size: 1rem;
+    outline: none;
+    resize: none;
+
+    ::placeholder {
+        color: #D1D1D1;
+    }
+`;
+
+export const FileDrop = styled.div`
+    width: 100%;
+    min-height: 15vh;
+    border: 1px solid #D1D1D1;
+    border-radius: 4px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #D1D1D1;
 `;
