d7c2df2d8cf570723c3fa4e2a24da32207f54a91
dpqlsn <a01037111209@gmail.com>
2025-11-06 12:25:01 +0900

    refactor(#81) :: 필수 글자 필수 아이콘으로 변경

--- src/widgets/student/monthlyTest/FormSection.tsx
+++ src/widgets/student/monthlyTest/FormSection.tsx
@@ -1,14 +1,26 @@
 import * as _ from './style';
-import type { FormSectionProps } from '@/shared/types/team'
+import Image from 'next/image';
+import type { FormSectionProps } from '@/shared/types/team';
 
 export default function FormSection({
     title,
     placeholders,
     isTextarea = false,
+    must = false,
 }: FormSectionProps) {
     return (
         <_.Wrapper>
-            <_.Title>{title}</_.Title>
+            <_.TitleWrapper>
+                <_.Title>{title}</_.Title>
+                {must && (
+                    <Image
+                        src="/assets/must.svg"
+                        alt="필수"
+                        width={10}
+                        height={10}
+                    />
+                )}
+            </_.TitleWrapper>
             {placeholders.map((placeholder, i) =>
                 isTextarea ? (
                     <_.Textarea key={i} placeholder={placeholder} />
