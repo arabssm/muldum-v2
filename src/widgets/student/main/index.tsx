78c2bbd2f70fb71eb74633b90f560bab985c4146
zuu3 <ojh27467242@gmail.com>
2025-11-07 20:22:14 +0900

    feat :: 메인 페이지 스켈레톤 UI 변경

--- src/widgets/student/main/index.tsx
+++ src/widgets/student/main/index.tsx
@@ -3,20 +3,19 @@
 import * as _ from "./style";
 import Link from "next/link";
 import { Menu, NoticeData } from "./data";
-import { useRouter } from "next/navigation"; 
+import { useRouter } from "next/navigation";
 import Slider from "@/shared/ui/slider";
 import { useState, useEffect } from "react";
-import MainSkeleton from "./skeleton";
+import { Skeleton } from "@/shared/ui/skeleton";
 
 export default function Main() {
     const router = useRouter();
-    const [isLoading, setIsLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
     const [isMounted, setIsMounted] = useState(false);
 
     useEffect(() => {
         setIsMounted(true);
-        setIsLoading(true);
-        
+
         const timer = setTimeout(() => {
             setIsLoading(false);
         }, 2000);
@@ -24,13 +23,17 @@ export default function Main() {
         return () => clearTimeout(timer);
     }, []);
 
-    if (!isMounted || isLoading) {
-        return <MainSkeleton />;
+    if (!isMounted) {
+        return null;
     }
 
     return (
         <_.Container>
-            <Slider />
+            {isLoading ? (
+                <Skeleton height="24vh" borderRadius="3.2px" />
+            ) : (
+                <Slider />
+            )}
             <_.Info>
                 <_.Wrapper>
                     <_.Title>메뉴</_.Title>
@@ -51,18 +54,29 @@ export default function Main() {
                         </_.Subtitle>
                     </_.Group>
                     <_.NoticeContainer>
-                        {NoticeData.map((item) => (
-                            <Link key={item.path} href={item.path}>
-                                <_.NoticeGroup>
-                                    {item.type === "new" ? (
-                                        <_.Badge bgColor="#FF9B62">{item.badge}</_.Badge>
-                                    ) : (
-                                        <_.Badge bgColor="#D1D1D1">{item.badge}</_.Badge>
-                                    )}
-                                    <_.Notice>{item.notice}</_.Notice>
-                                </_.NoticeGroup>
-                            </Link>
-                        ))}
+                        {isLoading ? (
+                            <>
+                                {Array.from({ length: 3 }).map((__, index) => (
+                                    <_.NoticeGroup key={index}>
+                                        <Skeleton width="50px" height="32px" borderRadius="10rem" />
+                                        <Skeleton width="200px" height="20px" />
+                                    </_.NoticeGroup>
+                                ))}
+                            </>
+                        ) : (
+                            NoticeData.map((item) => (
+                                <Link key={item.path} href={item.path}>
+                                    <_.NoticeGroup>
+                                        {item.type === "new" ? (
+                                            <_.Badge bgColor="#FF9B62">{item.badge}</_.Badge>
+                                        ) : (
+                                            <_.Badge bgColor="#D1D1D1">{item.badge}</_.Badge>
+                                        )}
+                                        <_.Notice>{item.notice}</_.Notice>
+                                    </_.NoticeGroup>
+                                </Link>
+                            ))
+                        )}
                     </_.NoticeContainer>
                 </_.Wrapper>
             </_.Info>
