8dbcbfcc9660d27397017b04a3bb919d99c03e60
dpqlsn <a01037111209@gmail.com>
2025-11-10 10:13:00 +0900

    feat(#92) :: 피드백 페이지 로직 생성

---
+++ src/widgets/teacher/feedback/index.tsx
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+
+export default function Feedback() {
+    return (
+        <Container>
+            피드백 페이지
+        </Container>
+    );
+}
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 1rem 0;
+    gap: 2.5rem;
+`;
