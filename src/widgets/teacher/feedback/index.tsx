cd80a03578d9a470e4fcfcebced494fec4fff7c3
dpqlsn <a01037111209@gmail.com>
2025-11-10 10:19:13 +0900

    feat(#92) :: 피드백 페이지 퍼블리싱

--- src/widgets/teacher/feedback/index.tsx
+++ src/widgets/teacher/feedback/index.tsx
@@ -1,17 +1,61 @@
 import styled from "@emotion/styled";
+import { useRouter } from "next/navigation";
+import { BtnPrimary, BtnSecondary } from "@/shared/ui/button";
 
 export default function Feedback() {
+    const router = useRouter();
+
     return (
         <Container>
-            피드백 페이지
+            <Wrapper>
+                <Title>피드백을 자유롭게 작성해주세요</Title>
+                <Input placeholder="피드백 내용을 작성해주세요." />
+            </Wrapper>
+            <BtnGroup>
+                <BtnSecondary onClick={() => router.push('/monthWatch')}>취소</BtnSecondary>
+                <BtnPrimary onClick={() => router.push('/monthCheck')}>반려</BtnPrimary>
+            </BtnGroup>
         </Container>
     );
 }
 
-export const Container = styled.div`
+const Container = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
     padding: 1rem 0;
-    gap: 2.5rem;
+    gap: 3rem;
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1.2rem;
+`;
+
+const BtnGroup = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: end;
+    gap: 1rem;
+`;
+
+const Title = styled.div`
+    font-size: 1.5rem;
+    font-weight: 600;
+`;
+
+const Input = styled.textarea`
+    width: 100%;
+    height: 30vh;
+    padding: 1rem;
+    border: 1px solid #D1D1D1;
+    border-radius: 4px;
+    font-size: 1rem;
+    outline: none;
+    resize: none;
+
+    ::placeholder {
+        color: #D1D1D1;
+    }
 `;
