bea168fb4c73c96494b92036408bce470427eefa
dpqlsn <a01037111209@gmail.com>
2025-11-12 19:30:30 +0900

    feat(#110) :: 선생님 월말평가 페이지 생성 후 조회 퍼블리싱

--- src/widgets/teacher/teamSpace/index.tsx
+++ src/widgets/teacher/teamSpace/index.tsx
@@ -1,9 +1,29 @@
 import * as _ from './style';
+import Team from '@/widgets/student/team';
+import { useState } from 'react';
 
-export default function Tteamspace(){
+type ItemType = "월말평가" | "학생추가";
+
+export default function Tteamspace() {
+    const Groups: ItemType[] = ["월말평가", "학생추가"];
+    const [activeGroup, setActiveGroup] = useState<ItemType>("월말평가");
+
     return (
         <_.Container>
+            <_.TabWrapper>
+                {Groups.map((group) => (
+                    <_.ClassText
+                        key={group}
+                        isActive={activeGroup === group}
+                        onClick={() => setActiveGroup(group)}
+                    >
+                        {group}
+                    </_.ClassText>
+                ))}
+            </_.TabWrapper>
 
+            {activeGroup === "월말평가" && <Team />}
+            {activeGroup === "학생추가" && <div>학생 추가 화면 내용</div>}
         </_.Container>
     );
 }
--- src/widgets/teacher/teamSpace/style.ts
+++ src/widgets/teacher/teamSpace/style.ts
@@ -1,9 +1,29 @@
 import styled from "@emotion/styled";
+import { Bar } from "@/shared/types";
 
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
+    gap: 2rem;
     padding: 1rem 0;
-    gap: 3rem;
+`;
+
+export const TabWrapper = styled.div`
+  display: flex;
+`;
+
+export const ClassText = styled.div<Bar>`
+  display: flex;
+  padding: 0.75rem;
+  font-size: 1.125rem;
+  font-weight: ${({ isActive }) => (isActive ? 500 : 400)};
+  cursor: pointer;
+  color: ${({ isActive }) => (isActive ? "#4B4B4B" : "#B2B2B2")};
+  border-bottom: 2.5px solid ${({ isActive }) => (isActive ? "#4B4B4B" : "#B2B2B2")};
+
+  &:hover {
+    background-color: ${({ isActive }) =>
+      isActive ? "transparent" : "#f5f5f5b6"};
+  }
 `;
