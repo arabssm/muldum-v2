3ca5dbae3ddc79b0843d5f538282d42d07b2b1a1
dpqlsn <a01037111209@gmail.com>
2025-11-17 04:45:53 +0900

    feat(#120) :: 슬라이더에도 정적인 공지 제거

--- src/shared/ui/slider/index.tsx
+++ src/shared/ui/slider/index.tsx
@@ -1,16 +1,19 @@
+"use client";
+
 import Image from "next/image";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import * as _ from "./style";
 import sliderSettings from "./Setting";
-import Data from "./data";
 import Link from "next/link";
+import { useState, useEffect } from "react";
+import { getNotices } from "@/shared/api/admin/notice";
 
 const NextArrow = (props: any) => {
   const { onClick } = props;
   return (
     <_.ArrowButton className="next-arrow" onClick={onClick}>
-      <Image src="/assets/Warrow.svg" alt="Next" width={24} height={24}  style={{ transform: "rotate(180deg)", opacity: 0.5 }} />
+      <Image src="/assets/Warrow.svg" alt="Next" width={24} height={24} style={{ transform: "rotate(180deg)", opacity: 0.5 }} />
     </_.ArrowButton>
   );
 };
@@ -19,38 +22,68 @@ const PrevArrow = (props: any) => {
   const { onClick } = props;
   return (
     <_.ArrowButton className="prev-arrow" onClick={onClick}>
-      <Image src="/assets/Warrow.svg" alt="Next" width={24} height={24} style={{ opacity: 0.5 }} />
+      <Image src="/assets/Warrow.svg" alt="Prev" width={24} height={24} style={{ opacity: 0.5 }} />
     </_.ArrowButton>
   );
 };
 
 export default function SliderComponent() {
+  const [notices, setNotices] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getNotices();
+      const arr = Array.isArray(res) ? res : res.data ?? [];
+      setNotices(arr);
+      setIsLoading(false);
+    })();
+  }, []);
+
   const settings = {
     ...sliderSettings,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
   };
 
+  if (isLoading) {
+    return (
+      <_.Container>
+        <div style={{ padding: "2rem 0", textAlign: "center" }}>로딩 중...</div>
+      </_.Container>
+    );
+  }
+
+  if (notices.length === 0) {
+    return (
+      <_.Container>
+        <div style={{ padding: "2rem 0", textAlign: "center", color: "#B2B2B2" }}>
+          현재 공지사항이 없습니다.
+        </div>
+      </_.Container>
+    );
+  }
+
   return (
     <_.Container>
       <_.StyledSlider {...settings}>
-        {Data.map((item, index) => (
-          <Link key={index} href={`/notice/${index+1}`}>
+        {notices.map((item, index) => (
+          <Link key={item.path || index} href={item.path || `/notice/${index + 1}`}>
             <_.SlideWrapper>
               <Image
                 src={"/assets/basicBG.svg"}
-                alt={item.title}
+                alt={item.notice}
                 width={1700}
                 height={320}
                 style={{ objectFit: "cover", borderRadius: "1rem" }}
               />
               <_.Overlay />
-              <_.Title>{item.title}</_.Title>
+              <_.Title>{item.notice}</_.Title>
               <_.Date>{item.date}</_.Date>
-              <_.SubTitle>{item.subtitle}</_.SubTitle>
-              <_.Ddate>{item.dDay}</_.Ddate>
+              {item.subtitle && <_.SubTitle>{item.subtitle}</_.SubTitle>}
+              {item.dDay && <_.Ddate>{item.dDay}</_.Ddate>}
               <_.Index>
-                {index + 1}/{Data.length}
+                {index + 1}/{notices.length}
               </_.Index>
             </_.SlideWrapper>
           </Link>
