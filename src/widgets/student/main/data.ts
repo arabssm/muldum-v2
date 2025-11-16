bfd611d4c493bbcde71d87520a7b05d7635d123f
123isi <123isi@naver.com>
2025-11-17 08:40:26 +0900

    api 연결

--- src/widgets/student/main/data.ts
+++ src/widgets/student/main/data.ts
@@ -4,3 +4,9 @@ export const Menu = [
     { title: "역대 동아리 보러가기", subtitle: "바로가기 >", path: "/clubs" },
     { title: "팀스페이스 보러가기", subtitle: "바로가기 >", path: "/team" },
 ];
+
+export const NoticeData = [
+    { path: "/notice/1", type: "new", badge: "NEW", notice: "공지사항 제목 1", date: "2024.01.01" },
+    { path: "/notice/2", type: "old", badge: "공지", notice: "공지사항 제목 2", date: "2024.01.02" },
+    { path: "/notice/3", type: "old", badge: "공지", notice: "공지사항 제목 3", date: "2024.01.03" },
+];
