456e90859d43e89b19308feaf7fea274d9691dab
123isi <123isi@naver.com>
2025-11-22 23:33:45 +0900

    feat :: useSearchParans를 suspense로 감싸기

--- src/app/monthWatch/page.tsx
+++ src/app/monthWatch/page.tsx
@@ -1,7 +1,12 @@
 "use client"
 
+import { Suspense } from "react";
 import MonthWatch from "@/widgets/teacher/monthWatch";
 
 export default function NoticeWatchPage() {
-  return <MonthWatch />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <MonthWatch />
+    </Suspense>
+  )
 }
--- src/app/reapply/page.tsx
+++ src/app/reapply/page.tsx
@@ -1,7 +1,12 @@
 "use client"
 
+import { Suspense } from "react";
 import Reapply from "@/widgets/student/reapply";
 
 export default function ReapplyPage() {
-  return <Reapply />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Reapply />
+    </Suspense>
+  )
 }
